914a51fd89087ef5622f0e4def55fae2266fc2fe
Pieter Brandwijk <pieter@pbrandwijk.com>
2026-01-02 09:37:26 +0100

    Add excercise 2.2

--- HSoM.hs
+++ HSoM.hs
@@ -31,3 +31,41 @@ circle p d = circle' p d 3
 
 circle' p d acc | acc == 0 = transpose 7 $ twoFiveOne p d
                 | otherwise = (twoFiveOne p d) :+: transpose 7 (circle' p d (acc-1))
+
+
+-- Exercise 2.2
+
+data BluesPitchClass = Ro | MT | Fo | Fi | MS deriving Show
+
+type BluesPitch = (BluesPitchClass, Octave)
+
+ro :: Octave -> Dur -> Music BluesPitch
+ro o d = note d (Ro, o)
+
+mt :: Octave -> Dur -> Music BluesPitch
+mt o d = note d (MT, o)
+
+fo :: Octave -> Dur -> Music BluesPitch
+fo o d = note d (Fo, o)
+
+fi :: Octave -> Dur -> Music BluesPitch
+fi o d = note d (Fi, o)
+
+ms :: Octave -> Dur -> Music BluesPitch
+ms o d = note d (MS, o)
+
+fromBlues :: Music BluesPitch -> Music Pitch
+fromBlues (Prim (Note d p)) = (Prim (Note d (translateBlues p)))
+fromBlues (Prim (Rest d)) = (Prim (Rest d))
+fromBlues (m1 :+: m2) = fromBlues m1 :+: fromBlues m2
+fromBlues (m1 :=: m2) = fromBlues m1 :=: fromBlues m2
+fromBlues (Modify ctrl m) = Modify ctrl (fromBlues m)
+
+translateBlues :: BluesPitch -> Pitch
+translateBlues (Ro, o) = (C, o)
+translateBlues (MT, o) = (Ef, o)
+translateBlues (Fo, o) = (F, o)
+translateBlues (Fi, o) = (G, o)
+translateBlues (MS, o) = (Bf, o)
+
+bluesTest d = ro 4 d :+: mt 4 d :+: fo 4 d :+: fi 4 d :+: ms 4 d
